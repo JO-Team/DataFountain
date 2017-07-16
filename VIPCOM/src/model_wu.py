945e760f5e2c77faef4ed2506a45563a7dd7dab4
GuidoPaul <Baoshenglin1994@gmail.com>
2017-07-16 19:07:06 +0800

    add notebook.

--- VIPCOM/src/model_wu.py
+++ VIPCOM/src/model_wu.py
@@ -7,22 +7,33 @@ import xgboost as xgb
 import datetime
 import scipy as sp
 
-train = pd.read_csv('../input/user_action_train.txt', sep="\t", header=None, parse_dates=[3])
+train = pd.read_csv(
+    '../input/user_action_train.txt', sep="\t", header=None, parse_dates=[3])
 goods = pd.read_csv('../input/goods_train.txt', sep="\t", header=None)
-test = pd.read_csv('../input/user_action_test_items.txt.', sep="\t", header=None)
+test = pd.read_csv(
+    '../input/user_action_test_items.txt.', sep="\t", header=None)
 
 # remove useless column of user_action_text_item.txt
 test = test.drop(test.columns[[2]], axis=1)
 
-train.rename(columns={0: 'uid', 1: 'spu_id', 2: 'action_type', 3: 'date'}, inplace=True)
-goods.rename(columns={0: 'spu_id_goods', 1: 'brand_id', .2: 'cat_id'}, inplace=True)
+train.rename(
+    columns={0: 'uid',
+             1: 'spu_id',
+             2: 'action_type',
+             3: 'date'}, inplace=True)
+goods.rename(
+    columns={0: 'spu_id_goods',
+             1: 'brand_id',
+             .2: 'cat_id'}, inplace=True)
 test.rename(columns={0: 'uid', 1: 'spu_id'}, inplace=True)
 
-train_goods = pd.merge(train, goods, left_on='spu_id', right_on='spu_id_goods', how='left')
+train_goods = pd.merge(
+    train, goods, left_on='spu_id', right_on='spu_id_goods', how='left')
 train_goods.rename(columns={5: 'cat_id'}, inplace=True)
 
 train_label = train['action_type']
-train_goods = train_goods.drop(['action_type', 'date', 'spu_id_goods', 'brand_id'], axis=1)
+train_goods = train_goods.drop(
+    ['action_type', 'date', 'spu_id_goods', 'brand_id'], axis=1)
 train_goods = train_goods.drop([2], axis=1)
 print(train_goods.head())
 
@@ -41,11 +52,17 @@ dtest = xgb.DMatrix(test)
 
 num_boost_rounds = 100
 
-cv_output = xgb.cv(xgb_params, dtrain, num_boost_round=num_boost_rounds, verbose_eval=50, show_stdv=False)
+cv_output = xgb.cv(
+    xgb_params,
+    dtrain,
+    num_boost_round=num_boost_rounds,
+    verbose_eval=50,
+    show_stdv=False)
 print(cv_output[['train-rmse-mean', 'test-rmse-mean']])
 
 print(len(cv_output))
-model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round=num_boost_rounds)
+model = xgb.train(
+    dict(xgb_params, silent=0), dtrain, num_boost_round=num_boost_rounds)
 
 y_predict = model.predict(dtest)
 model_output = pd.DataFrame({'weight': y_predict})
